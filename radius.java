22b1660fb4a77d6d89f28314a04be0abd559f515
MANIKANDAN-IT21 <manikandan.it21@bitsathy.ac.in>
2024-05-15 18:37:21 +0530

    java

---
+++ radius.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+      double radius=7.5;
+      System.out.println("Area "+22/7*radius*radius);//22/7*r*r=area of the circle
+      System.out.println("parameter "+2*22/7*radius*radius); //2pir^2=parameter of the circle
+    }
+}
